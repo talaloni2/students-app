64c86752946db63cfff71e667521bed82e14a438
Tal Aloni <tal.aloni@planckresolution.com>
2022-12-04 20:49:07 +0200

    moved MainActivity to activities package

--- app/src/main/java/com/example/studentsapp/activities/MainActivity.java
+++ app/src/main/java/com/example/studentsapp/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.studentsapp;
+package com.example.studentsapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.studentsapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
